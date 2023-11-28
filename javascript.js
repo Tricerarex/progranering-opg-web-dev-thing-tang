2a61ef56950f9c6c134738b3855e92e1c0964692
Rowan-Slugaye <83877730+Rowan-Slugaye@users.noreply.github.com>
2023-11-28 11:52:23 +0100

    modified:   index.html 	modified:   javascript.js 	modified:   styel.css

--- javascript.js
+++ javascript.js
@@ -1,9 +1,14 @@
 
+let root = document.documentElement;
+
+
 
 function nav_meny(){
 
 }
 
+
 function mode_controle(){
-    
+    var element = document.body;
+    element.classList.toggle("light-mode");
 }
