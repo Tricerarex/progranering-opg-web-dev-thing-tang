809e924fb6bf6c57560f9d29488d0975e03e17df
Rowan-Slugaye <83877730+Rowan-Slugaye@users.noreply.github.com>
2023-12-22 16:13:44 +0100

    modified:   imigas/favicon.ico 	modified:   javascript/javascript_game3.js 	modified:   styel/styel_game3.css

---
+++ javascript/javascript_game3.js
@@ -0,0 +1,9 @@
+
+// Started on by Noah
+
+
+// Get the canvas element and info about it
+var canvas = document.getElementById('Game-window');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+var context = canvas.getContext('2d');
